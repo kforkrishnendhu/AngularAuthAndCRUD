d279c22d7f58b0b1d5707a3004c1c54a25708436
Krishnendhu Arun <kforkrishnendhu@gmail.com>
2024-05-15 22:02:22 +0530

    JWT and Auth guards

--- AngularAuthAPI/AngularAuthAPI/Controllers/UserController.cs
+++ AngularAuthAPI/AngularAuthAPI/Controllers/UserController.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AngularAuthAPI.Context;
 using AngularAuthAPI.Helpers;
 using AngularAuthAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -40,9 +44,11 @@ namespace AngularAuthAPI.Controllers
             {
                 return BadRequest(new { Message = "Password is incorrect" });
             }
+            user.Token = CreateJwt(user);
 
             return Ok(new
             {
+                Token=user.Token,
                 Message="Login Success!"
             });
         }
@@ -107,11 +113,62 @@ namespace AngularAuthAPI.Controllers
                 sb.Append("Minimum password length should be 8" + Environment.NewLine);
             if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]")))
                 sb.Append("Password should be alphanumeric"+ Environment.NewLine);
-            if (!Regex.IsMatch(password, "[~,!,@,#,$,%,^,&,*,(,),_,-,+,=,{,},|,\\,<,>,.,?,/,\n]"))
+            if (!Regex.IsMatch(password, "[~,!,@,#,$,%,^,&,*,(,),_,+,=,{,},|,\\,<,>,.,?,/,\n]"))
                 sb.Append("Password should contain special characters"+Environment.NewLine);
 
             return sb.ToString();
         }
+
+        private string CreateJwt(User user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("secretkeyofmine.....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role,user.Role),
+                new Claim(ClaimTypes.Name,$"{user.FirstName} {user.LastName}"),
+                 new Claim("username", user.UserName)
+            });
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires=DateTime.Now.AddDays(1),
+                SigningCredentials=credentials
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+        [Authorize]   //protect this API endopint from being accessed without authorisation
+        [HttpGet]
+        public async Task<ActionResult<User>> GetAllUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        //[Authorize]
+        //[Route("api/[controller]/details")]
+        //[HttpGet]
+        //public async Task<ActionResult<User>> getUserDetails()
+        //{
+        //    var usernameClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "username");
+        //    if (usernameClaim == null)
+        //    {
+        //        return Unauthorized(); 
+        //    }
+
+        //    var username = usernameClaim.Value;
+        //    // Use the username to fetch the user data
+        //    var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == username);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(user);
+        //}
+
     }
 }
 
--- AngularAuthAPI/AngularAuthAPI/Program.cs
+++ AngularAuthAPI/AngularAuthAPI/Program.cs
@@ -1,6 +1,10 @@
-﻿using AngularAuthAPI.Context;
+﻿using System.Text;
+using AngularAuthAPI.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +27,24 @@ builder.Services.AddCors(options =>
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secretkeyofmine.....")),
+        ValidateAudience = false,
+        ValidateIssuer=false
+    };
 });
 
 var app = builder.Build();
